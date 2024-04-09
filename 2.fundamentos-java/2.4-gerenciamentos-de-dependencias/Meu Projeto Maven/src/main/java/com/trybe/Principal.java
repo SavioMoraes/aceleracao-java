63f56ca162fd88da091fa34ac4d72d7f9dcc5519
Savio Moraes <smmsavio@hotmail.com>
2024-04-09 19:14:13 -0300

    feat: javadoc

--- 2.fundamentos-java/2.4-gerenciamentos-de-dependencias/Meu Projeto Maven/src/main/java/com/trybe/Principal.java
+++ 2.fundamentos-java/2.4-gerenciamentos-de-dependencias/Meu Projeto Maven/src/main/java/com/trybe/Principal.java
@@ -2,8 +2,16 @@ package com.trybe;
 
 import com.google.common.base.Joiner;
 
+/**
+ * The type Principal.
+ */
 public class Principal {
 
+    /**
+     * The entry point of application.
+     *
+     * @param args the input arguments
+     */
     public static void main(String[] args) {
         String result = Joiner.on(", ").skipNulls().join("Olá", null, "mundo");
         System.out.println(result);
