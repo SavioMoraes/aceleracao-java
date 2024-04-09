e7bc6e050ef7bedc553c6413e2acc3e8738fc3fc
Savio Moraes <smmsavio@hotmail.com>
2024-04-09 18:37:36 -0300

    2.4 - gerenciamento de dependencias

---
+++ 2.fundamentos-java/2.4-gerenciamentos-de-dependencias/Meu Projeto Maven/src/main/java/com/trybe/Principal.java
@@ -0,0 +1,11 @@
+package com.trybe;
+
+import com.google.common.base.Joiner;
+
+public class Principal {
+
+    public static void main(String[] args) {
+        String result = Joiner.on(", ").skipNulls().join("Olá", null, "mundo");
+        System.out.println(result);
+    }
+}
